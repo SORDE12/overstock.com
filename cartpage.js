43135f129c1b07c37bf84424df608a62dcfad868
SORDE12 <108116297+SORDE12@users.noreply.github.com>
2022-09-29 07:57:25 +0530

    added

---
+++ cartpage.js
@@ -0,0 +1,192 @@
+
+
+
+likedData=[
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/30edd1359639179ab267545062a456d754dfdbac/SAFAVIEH-Handmade-Capri-Ilianka-Wool-Rug.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$35",
+        "description": "Name:SAFAVIEH Handmade Capri Ilianka Wool Rug",
+        "rating": "Rating:4.5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/50de42d7aefa890a8cb16948d26d201b4f6919ca/LUCID-Comfort-Collection-10-inch-Gel-Memory-Foam-Mattress.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$245",
+        "description": "Name:LUCID Comfort Collection Firm 10-inch Gel Memory Foam Mattress",
+        "rating": "Rating:4.5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/e73d21339290da4dbc0977e71f1a15b3667de1d8/Elle-Decor-Lulu-Bent-Metal-Side-Table.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$88",
+        "description": "Name:Elle Decor Lulu Round Hourglass Metal Side Table, Small Modern Accent Nightstand",
+        "rating": "Rating:3 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/576a0674d5cb51ff68024dd19bdaa823117b5f76/F.-Scott-Fitzgerald-Sterling-Silver-Throw-Pillow.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$42",
+        "description": "Name:F. Scott Fitzgerald Sterling Silver Throw Pillow",
+        "rating": "Rating:5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/6ec30be262d533773c41ac91702cd52b65025de0/Wallniture-Palla-Set-of-4-Ball-Holder%2C-Storage-Rack-for-Basketball%2C-Football%2C-Volleyball%2C-Soccer-Ball.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$30",
+        "description": "Name:Wallniture Palla Set of 4 Ball Holder, Storage Rack for Basketball, Football, Volleyball, Soccer Ball",
+        "rating": "Rating:4.5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/598b3df2eeebafa94d2c27805d3c02a7f8d94ddf/Rustico-Wine-Rack-with-Stemware-Holder.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$100",
+        "description": "Name:Rustic State Wood Wall Mounted Wine Bottle and Glass Rack in Walnut",
+        "rating": "Rating: Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/28b8866ac697fbf23acf3efd1aafc3dbb5ca9e8a/Two-Tier-Ledge-Shelf-Wall-Organizer-with-Five-Hanging-Hooks---White.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$50",
+        "description": "Name:Two-Tier Ledge Shelf Wall Organizer with Five Hanging Hooks - White",
+        "rating": "Rating:5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/e38dfe9692e9c309410639fee8e1faac78287e9f/SAFAVIEH-Handmade-Capri-Sigrun-Contemporary-Wool-Rug.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$58",
+        "description": "Name:SAFAVIEH Handmade Capri Sigrun Contemporary Wool Rug",
+        "rating": "Rating:5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/359594edb74ad45e48f7dcc00694d74bcd8e1434/SAFAVIEH-Handmade-Capri-Diyana-Contemporary-Wool-Rug.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$63",
+        "description": "Name:SAFAVIEH Handmade Capri Diyana Contemporary Wool Rug",
+        "rating": "Rating:4.5 Star"
+    }
+]
+
+viewed=[
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/56c3c9f2f57395f2743760aa725994c8f0666852/SAFAVIEH-Handmade-Capri-Berntraud-Contemporary-Wool-Rug.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$60",
+        "description": "Name:SAFAVIEH Handmade Capri Berntraud Contemporary Wool Rug",
+        "rating": "Rating:5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/6ec30be262d533773c41ac91702cd52b65025de0/Wallniture-Palla-Set-of-4-Ball-Holder%2C-Storage-Rack-for-Basketball%2C-Football%2C-Volleyball%2C-Soccer-Ball.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$29",
+        "description": "Name:Wallniture Palla Set of 4 Ball Holder, Storage Rack for Basketball, Football, Volleyball, Soccer Ball",
+        "rating": "Rating:4 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/46a2619bf7e2515d8d79a4b5e658a5a3f78d02d1/LUCID-Comfort-Collection-10-inch-Luxury-Gel-Memory-Foam-Mattress.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$239",
+        "description": "Name:LUCID Comfort Collection 10-inch Luxury Gel Memory Foam Mattress",
+        "rating": "Rating:4.7 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/17928359/Keter-Capri-80-Gallon-Resin-Deck-Box-768077e8-b25b-420a-a25b-654d2bd6d9f2_320.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$108",
+        "description": "Name:Keter Capri 80 Gallon Resin Deck Box",
+        "rating": "Rating:4 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/30109999/Christmas-Quilt-Bedspread-Set-bd54c37c-10bd-4c6a-9298-1081c1317023_320.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$63",
+        "description": "Name:Plaid Patchwork Christmas Quilt Bedding Set",
+        "rating": "Rating:5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/db13b063ceefcf2b0cefb9e41d3fe0dbace3b792/Great-Bay-Home-Reversible-Stripe-and-Dot-Quilt-Set.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$35",
+        "description": "Name:Great Bay Home All-Season Reversible Quilt Set With Shams",
+        "rating": "Rating:3 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/cadf404a9bad13152d5d8a75e496339947cab2a3/BNK-24%22-30%22-36%22-40%22-48%22-Modern-Single-Sink-Bathroom-Vanity-with-Ceramic-Top-Set.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$553",
+        "description": "Name:BNK 24\"/30\"/36\"/40\"/48\" Modern Single Sink Bathroom Vanity with Ceramic Top Set",
+        "rating": "Rating:4.5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/0b23106622c648e7c9a4200879e70b737206f597/VEIKOUS-Oversized-Bathroom-Medicine-Cabinet-Wall-Mounted-Storage-with-Mirrors-and-Shelves.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$97",
+        "description": "Name:VEIKOUS Oversized Bathroom Medicine Cabinet Wall Mounted Storage with Mirrors",
+        "rating": "Rating:4.5 Star"
+    },
+    {
+        "image": "https://ak1.ostkcdn.com/images/products/is/images/direct/28058f500b5bcb038398948d802cc2d89d1352fb/VEIKOUS-Over-The-Toilet-Storage-Cabinet-Bathroom-Organizer-with-Shelf-and-Cupboard.jpg?impolicy=mediumlow",
+        "amt": "Sale Amount:$98",
+        "description": "Name:VEIKOUS Over-The-Toilet Storage Cabinet Bathroom Organizer with Shelf and Cupboard",
+        "rating": "Rating:4 Star"
+    }
+]
+
+let appendliked=(data)=>{
+
+    let loved=document.getElementById('lovedItems')
+
+    data.forEach((ele)=>{
+
+        let img=document.createElement('img')
+        img.src=ele.image
+
+        let amt=document.createElement('h3')
+        amt.innerHTML=ele.amt
+
+        let des=document.createElement('p')
+        des.innerHTML=ele.description
+
+        let rating=document.createElement('p')
+        rating.innerHTML=ele.rating
+
+        let btn=document.createElement("button")
+
+        btn.innerText='Add To Cart'
+
+        btn.setAttribute('class',"div1")
+
+        btn.addEventListener('click',function(){
+            addToCart()
+        })
+
+        let div=document.createElement('div')
+        div.append(img,btn,amt,des,rating)
+
+        loved.append(div)
+    })
+}
+appendliked(likedData)
+
+
+
+
+let appendrecent=(data)=>{
+
+    let recent=document.getElementById('recentView')
+
+    data.forEach((ele)=>{
+
+        let img=document.createElement('img')
+        img.src=ele.image
+
+        let amt=document.createElement('h3')
+        amt.innerHTML=ele.amt
+
+        let des=document.createElement('p')
+        des.innerHTML=ele.description
+
+        let rating=document.createElement('p')
+        rating.innerHTML=ele.rating
+
+        let btn=document.createElement("button")
+
+        btn.innerText='Add To Cart'
+
+        btn.setAttribute('class',"div1")
+
+        btn.addEventListener('click',function(){
+            addToCart()
+        })
+
+        let div=document.createElement('div')
+        div.append(img,btn,amt,des,rating)
+
+        recent.append(div)
+    })
+}
+appendrecent(viewed)
+
